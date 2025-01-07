3a83a480d5da645c039ebf7f78292eb6bc4ff0ca
AlfCikal <alfath.c1k4l@gmail.com>
2025-01-07 22:03:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	basePath: "./Hops-beat-e-commerce", // Replace <repository-name> with your actual GitHub repository name
-	assetPrefix: "./Hops-beat-e-commerce", // Ensure static files are served from the correct path
+	basePath: "/Hops-beat-e-commerce", // Replace <repository-name> with your actual GitHub repository name
+	assetPrefix: "/Hops-beat-e-commerce", // Ensure static files are served from the correct path
 	reactStrictMode: true,
 };
 
